801661a58062ec7096cd8b4981fe5c9e3d3973d9
Foam-Ranther <yashzzz321@gmail.com>
2025-10-20 10:47:22 +0530

    created a function to play and added the functionality to play rounds

--- games/simple_programs/rock_paper_scissors.js
+++ games/simple_programs/rock_paper_scissors.js
@@ -2,11 +2,11 @@ const CHOICES = ["rock", "paper", "scissors"];
 
 function randomChoice() {
   const randomIndex = Math.floor(Math.random() * CHOICES.length);
-  console.log(CHOICES[randomIndex]);
-  console.log(randomIndex);
+  return CHOICES[randomIndex]; 
 }
 
 function displayResult(p1Choice, p2Choice, result) {
+  console.log("\n"); 
   console.log("player 1 choose : ", p1Choice);
   console.log("player 2 choose : ", p2Choice);
   console.log("result : ", result);
@@ -18,7 +18,7 @@ function checkWinner(p1Choice, p2Choice) {
   const p1ChoiceIndex = CHOICES.indexOf(p1Choice);
 
   if (p2Choice === winningChoices[p1ChoiceIndex]) {
-    return displayResult(p1Choice, p2Choice, "player 1 won");
+    return displayResult(p1Choice, p2Choice, "player 2 won");
   }
 
   const p2ChoiceIndex = CHOICES.indexOf(p2Choice);
@@ -32,15 +32,22 @@ function checkWinner(p1Choice, p2Choice) {
 
 function play() {
   const rounds = prompt("How many rounds do you wanna play ? "); 
-  
+
+  console.log(`you choose to play for ${rounds} rounds \n`); 
+
   for (let index = 0; index < rounds; index++) {
     const p1Choice = prompt("player 1 (enter your choice) : "); 
-    const p2Choice = prompt("player 2 (enter your choice) : "); 
+    const p2Choice = randomChoice();
     checkWinner(p1Choice, p2Choice); 
   }
+  
+  if (confirm()) {
+    play(); 
+  }
+
 }
 
-function main() {
+function testCheckWinner() {
   checkWinner("rock", "paper");
   checkWinner("rock", "scissors");
   checkWinner("rock", "rock");
@@ -50,6 +57,11 @@ function main() {
   checkWinner("scissors", "paper");
   checkWinner("scissors", "rock");
   checkWinner("scissors", "scissors");
+}
+
+function main() {
+  // testCheckWinner(); 
+  play(); 
 }
 
 main(); 
