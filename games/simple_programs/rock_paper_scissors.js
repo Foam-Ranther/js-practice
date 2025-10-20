b6e3463ad2837d33798d78717cf0e71e95d3ae0b
Foam-Ranther <yashzzz321@gmail.com>
2025-10-20 10:04:06 +0530

    wrote a function to produce randomChoices and displayResult

--- games/simple_programs/rock_paper_scissors.js
+++ games/simple_programs/rock_paper_scissors.js
@@ -1,38 +1,55 @@
-const CHOICES = ["rock", "paper", "scissors"]; 
+const CHOICES = ["rock", "paper", "scissors"];
 
 function randomChoice() {
-  const randomIndex = Math.floor(Math.random() * CHOICES.length); 
-  console.log(CHOICES[randomIndex]); 
-  console.log(randomIndex); 
+  const randomIndex = Math.floor(Math.random() * CHOICES.length);
+  console.log(CHOICES[randomIndex]);
+  console.log(randomIndex);
+}
+
+function displayResult(p1Choice, p2Choice, result) {
+  console.log("player 1 choose : ", p1Choice);
+  console.log("player 2 choose : ", p2Choice);
+  console.log("result : ", result);
+  console.log("\n"); 
 }
 
 function checkWinner(p1Choice, p2Choice) {
-  const winningChoices = ["paper", "scissors", "rock"]; 
-  const p1ChoiceIndex = CHOICES.indexOf(p1Choice); 
+  const winningChoices = ["paper", "scissors", "rock"];
+  const p1ChoiceIndex = CHOICES.indexOf(p1Choice);
 
   if (p2Choice === winningChoices[p1ChoiceIndex]) {
-    return "player 2 won"; 
+    return displayResult(p1Choice, p2Choice, "player 1 won");
   }
 
-  const p2ChoiceIndex = CHOICES.indexOf(p2Choice); 
+  const p2ChoiceIndex = CHOICES.indexOf(p2Choice);
 
   if (p1Choice === winningChoices[p2ChoiceIndex]) {
-    return "player 1 won"; 
+    return displayResult(p1Choice, p2Choice, "player 1 won");
   }
 
-  return "it was draw"; 
+  return displayResult(p1Choice, p2Choice, "It was draw");
+}
+
+function play() {
+  const rounds = prompt("How many rounds do you wanna play ? "); 
+  
+  for (let index = 0; index < rounds; index++) {
+    const p1Choice = prompt("player 1 (enter your choice) : "); 
+    const p2Choice = prompt("player 2 (enter your choice) : "); 
+    checkWinner(p1Choice, p2Choice); 
+  }
 }
 
 function main() {
-  console.log(checkWinner("rock", "paper")); 
-  console.log(checkWinner("rock", "scissors")); 
-  console.log(checkWinner("rock", "rock")); 
-  console.log(checkWinner("paper", "paper")); 
-  console.log(checkWinner("paper", "rock")); 
-  console.log(checkWinner("paper", "scissors")); 
-  console.log(checkWinner("scissors", "paper")); 
-  console.log(checkWinner("scissors", "rock")); 
-  console.log(checkWinner("scissors", "scissors")); 
+  checkWinner("rock", "paper");
+  checkWinner("rock", "scissors");
+  checkWinner("rock", "rock");
+  checkWinner("paper", "paper");
+  checkWinner("paper", "rock");
+  checkWinner("paper", "scissors");
+  checkWinner("scissors", "paper");
+  checkWinner("scissors", "rock");
+  checkWinner("scissors", "scissors");
 }
 
 main(); 
