28e3e8cd1f2e7d31a09b9425cd5f1cada2f73a64
Foam-Ranther <yashzzz321@gmail.com>
2025-10-21 12:11:58 +0530

    Refactor displayArray and add padding functions

--- games/simple_programs/tik_tac_toe_2.js
+++ games/simple_programs/tik_tac_toe_2.js
@@ -1,19 +1,27 @@
-function displayArray(array) {
-  console.log("\n"); 
-  let columnString = ""; 
-
-  for (let index = 1; index <= array.length; index++) {
-    columnString += ` ${array[index - 1]} |`; 
-
-    if (index % 3 === 0 ) {
-      console.log(columnString); 
-      columnString = ""; 
-      console.log("---|---|---|")
-    }
-  }
-
-  console.log("\n"); 
+function pad(count, string, char) {
+  let paddedString = string.padStart(string.length + count ,char); 
+  return paddedString; 
 }
+
+function convertArrayToString(array) {
+  const tempArray = []
+  for (let index = 0; index < array.length; index++) {
+    let columnString =array[index].join("|"); 
+    tempArray.push(pad(5, columnString, " ")); 
+  }
+  
+  return tempArray.join("\n     ----|----|----\n"); 
+}
+
+function displayArray(array) {
+  const convertedString = convertArrayToString(array); 
+  // console.log(convertedString); 
+  const paddedString =  pad(5, convertedString, "\n");
+  console.log(paddedString);  
+  console.log("\n \n")
+}
+
+
 
 function isWinningCombination(array, string, char) {
   for(let index = 0; index < string.length; index++) {
@@ -39,10 +47,24 @@ function isPlayerWinner(array, char) {
   return isWinner; 
 }
 
+function testDisplayArray() {
+  const array = [
+    [" ❌ ", " ❌ ", " ❌ "], 
+    [" ❌ ", " ❌ ", " ❌ "], 
+    [" 🟢 ", " 🟢 ", " 🟢 "], 
+  ]; 
+  
+  displayArray(array); 
+}
 
 function main() {
-  const array = ["*", "*", "7", "*", "*", "*", "6", "7", "8"]; 
-  displayArray(array);
-  console.log(isPlayerWinner(array, "*")); 
+  const array = [
+    ["0", "1", "2"], 
+    ["3", "4", "5"], 
+    ["6", "7", "8"], 
+  ]; 
+  // displayArray(array);
+  // console.log(isPlayerWinner(array, "*")); 
+  testDisplayArray(); 
 }
 main(); 
