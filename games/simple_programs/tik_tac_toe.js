d6b0099b53dc6e3441551e80c2abba401e1aa543
Foam-Ranther <yashzzz321@gmail.com>
2025-10-21 13:37:46 +0530

    Update tik_tac_toe.js

--- games/simple_programs/tik_tac_toe.js
+++ games/simple_programs/tik_tac_toe.js
@@ -1,16 +1,48 @@
 function displayArray(array) {
+  console.log("\n"); 
   let columnString = ""; 
+
   for (let index = 1; index <= array.length; index++) {
-    columnString += array[index - 1]; 
+    columnString += ` ${array[index - 1]} |`; 
+
     if (index % 3 === 0 ) {
       console.log(columnString); 
       columnString = ""; 
+      console.log("---|---|---|")
     }
   }
+
+  console.log("\n"); 
+}
+
+function isWinningCombination(array, string, char) {
+  for(let index = 0; index < string.length; index++) {
+    if (array[string[index]] !== char) {
+      return false; 
+    }
+  }
+
+  return true; 
+}
+
+function isPlayerWinner(array, char) { 
+  let isWinner = false; 
+  const winCombination = ["012", "345", "678", "036", "147", "258", "048", "246"]; 
+
+  for (let index = 0; index < winCombination.length; index++) {
+    isWinner = isWinningCombination(array, winCombination[index], char);
+    if (isWinner) {
+      return isWinner; 
+    } 
+  }
+
+  return isWinner; 
 }
 
 function main() {
-  const array = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]; 
+  const array = ["*", "*", "7", "*", "*", "*", "6", "7", "8"]; 
   displayArray(array);
+  console.log(isPlayerWinner(array, "*")); 
 }
+
 main(); 
