2d77c7d5cf108c627c0d8d443dbc380bb67d356e
Foam-Ranther <yashzzz321@gmail.com>
2025-10-24 10:24:16 +0530

    Update tik_tac_toe.js

--- games/simple_programs/tik_tac_toe.js
+++ games/simple_programs/tik_tac_toe.js
@@ -1,48 +1,139 @@
-function displayArray(array) {
-  console.log("\n"); 
-  let columnString = ""; 
+const WIN_COMBINATIONS = ["012", "345", "678",
+  "036", "147", "258",
+  "048", "246"];
 
-  for (let index = 1; index <= array.length; index++) {
-    columnString += ` ${array[index - 1]} |`; 
+const BOARD = [
+  " 1  ", " 2  ", " 3 ",
+  " 4  ", " 5  ", " 6 ",
+  " 7  ", " 8  ", " 9 "
+];
 
-    if (index % 3 === 0 ) {
-      console.log(columnString); 
-      columnString = ""; 
-      console.log("---|---|---|")
+function displayBoard() {
+  const tempArray = [];
+  let columnString = "\n";
+
+  for (let index = 1; index <= BOARD.length; index++) {
+    columnString += ` ${BOARD[index - 1]} |`;
+
+    if (index % 3 === 0) {
+      tempArray.push(columnString.slice(0, columnString.length - 1));
+      columnString = "";
     }
   }
-
-  console.log("\n"); 
+  const finalString = tempArray.join("\n------|------|------\n");
+  console.log(`\n\n${finalString}\n\n`);
 }
 
-function isWinningCombination(array, string, char) {
-  for(let index = 0; index < string.length; index++) {
-    if (array[string[index]] !== char) {
-      return false; 
-    }
+function isValidInput(playerInput) {
+  return playerInput > 0 && playerInput < 10;
+}
+
+function getInput(playerName) {
+  console.log("Enter the block number in order to chose the block");
+
+  let playerInput = prompt(`Enter your choice ${playerName} : `);
+  playerInput = parseInt(playerInput);
+
+  if (isValidInput(playerInput)) {
+    return playerInput;
   }
 
-  return true; 
+  console.log("❌ Invalid user Input, Try again \n");
+  return getInput(playerName);
 }
 
-function isPlayerWinner(array, char) { 
-  let isWinner = false; 
-  const winCombination = ["012", "345", "678", "036", "147", "258", "048", "246"]; 
+function isBlockEmpty(playerChoice) {
+  return BOARD[playerChoice] !== " ❌ " && BOARD[playerChoice] !== " 🟢 ";
+}
 
-  for (let index = 0; index < winCombination.length; index++) {
-    isWinner = isWinningCombination(array, winCombination[index], char);
+function getChosenBlockPosition(playerName) {
+  const playerChoice = getInput(playerName);
+
+  if (isBlockEmpty(BOARD, playerChoice)) {
+    return playerChoice;
+  }
+
+  console.log("Position has already been occupied \n");
+  return getChosenBlockPosition(playerName, BOARD);
+}
+
+function isWinningCombination(string, char) {
+  const zerothIndex = parseInt(string[0]);
+  const firstIndex = parseInt(string[1]);
+  const secondthIndex = parseInt(string[2]);
+
+  return BOARD[zerothIndex] === char && BOARD[firstIndex] === char &&
+    BOARD[secondthIndex] === char;
+}
+
+function isPlayerWinner(char) {
+  let isWinner = false;
+
+  for (let index = 0; index < WIN_COMBINATIONS.length; index++) {
+    isWinner = isWinningCombination(WIN_COMBINATIONS[index], char);
     if (isWinner) {
-      return isWinner; 
-    } 
+      return isWinner;
+    }
   }
 
-  return isWinner; 
+  return isWinner;
+}
+
+function playTurn(playerName, char) {
+  const playerChoice = getChosenBlockPosition(playerName);
+  const chosenPosition = playerChoice - 1;
+
+  BOARD[chosenPosition] = char;
+  displayBoard();
+}
+
+function playAccordingToTurn(currentTurn, p1Name, p2Name) {
+  if (currentTurn % 2 === 0) {
+    return playTurn(p2Name, " 🟢 ");
+  }
+
+  return playTurn(p1Name, " ❌ ");
+}
+
+function startGame(p1Name, p2Name) {
+  let turns = 9;
+  let currentTurn = 1;
+  displayBoard();
+
+  playAccordingToTurn(currentTurn, p1Name, p2Name);
+  currentTurn += 1;
+
+  while (currentTurn <= turns) {
+    playAccordingToTurn(currentTurn, p1Name, p2Name);
+    
+    if (isPlayerWinner(char)) {
+      console.log(`${playerName} won the game 🎉🎉 \n`);
+      return;
+    }
+    currentTurn += 1;
+  }
+
+  console.log("It was draw.");
+}
+
+function config() {
+  const p1Name = prompt("Enter your name (p1) : ");
+  const p2Name = prompt("Enter your name (p2) : ")
+
+  console.log(`${p1Name} marker : " ❌ "`);
+  console.log(`${p2Name} marker : " 🟢 "`);
+
+  startGame(p1Name, p2Name);
+
+  const wannaPlayAgain = confirm("Do you wanna play again ? ");
+  if (wannaPlayAgain) {
+    config();
+  }
 }
 
 function main() {
-  const array = ["*", "*", "7", "*", "*", "*", "6", "7", "8"]; 
-  displayArray(array);
-  console.log(isPlayerWinner(array, "*")); 
+  config();
+  console.log("Thanks for playing.");
 }
 
 main(); 
